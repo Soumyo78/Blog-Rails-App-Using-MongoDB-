d0c0890d8b1c8d30127ae81ec10560f0ff0096e9
Soumyo78 <soumyo78@yahoo.com>
2022-12-08 15:08:09 +0530

    Some code changes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,9 @@
 class UserSerializer < BaseSerializer
   attributes :id, :name, :email, :time_stamp, :avatar_url
 
-  has_many :posts, unless: :exclude_assc?
+  # has_many :posts, unless: :exclude_assc?
 
-  def exclude_assc?
-    instance_options[:serializer_context_class]
-  end
+  # def exclude_assc?
+  #   instance_options[:serializer_context_class]
+  # end
 end
